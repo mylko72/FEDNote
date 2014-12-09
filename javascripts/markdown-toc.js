8c42370de401a5dc477e7e903854d37e75173a91
mylko <mypmk@naver.com>
2014-12-09 21:40:36 +0900

    update angular-app.html and js, css

---
+++ javascripts/markdown-toc.js
@@ -0,0 +1,43 @@
+/**
+ * User: powerumc
+ * Date: 2014. 5. 18.
+ * Time: 오후 8:39
+ */
+
+
+$(function() {
+	$(function () {
+		$.ajax({
+			url: "/markdown/angular-app.md",
+			type: "GET",
+			success: function (data) {
+				var html = marked(data);
+				$("#preview").html(html);
+
+				var toc = $("#toc").tocify({
+					context: "#preview",
+					selectors: "h2, h3, h4, h5",
+					showEffect: "slideDown",
+					hideEffect: "slideUp",
+					theme: "jqueryui",
+					hashGenerator: "pretty",
+					highlightOffset: 40});
+
+
+				$("code").addClass("prettyprint");
+
+				prettyPrint();
+
+				$(document).ready(function () {
+					$('[data-toggle=offcanvas]').click(function () {
+						$('.row-offcanvas').toggleClass('active');
+					});
+				});
+
+				$("#title").text($("#preview h1:first").text());
+			}
+		});
+	});
+
+});
+
